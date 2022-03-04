57ce9cd65adc85c6dad0a8077cf3b7640f51e8c5
Matthias Monnier <matthias.monnier@gmail.com>
2022-03-04 11:28:04 +0100

    Add login logic on backend

---
+++ backend/middlewares/authMiddleware.js
@@ -0,0 +1,32 @@
+const jwt = require('jsonwebtoken');
+const asyncHandler = require('express-async-handler');
+const User = require('../models/userModel');
+
+const protect = asyncHandler(async (req, res, next) => {
+  let token;
+
+  const auth = req.headers.authorization;
+  if (auth && auth.startsWith('Bearer')) {
+    try {
+      // Get token from header
+      token = auth.split('')[1];
+      // Let's verify it
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      // Get user from token
+      req.user = await User.findById(decoded.id).select('-password');
+      next();
+    } catch (error) {
+      console.log(error);
+      res.status(401);
+      throw new Error('Not Authorized');
+    }
+  }
+
+  if (!token) {
+    console.log(error);
+    res.status(401);
+    throw new Error('Not Authorized');
+  }
+});
+
+module.exports = { protect };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,11 @@
 // Routes for /api/users/
 const express = require('express');
 const router = express.Router();
+const { protect } = require('../middlewares/authMiddleware');
 const { registerUser, loginUser } = require('../controllers/userController');
 
 router.post('/', registerUser);
-
 router.post('/login', loginUser);
+//router.post('/login', protect, loginUser);
 
 module.exports = router;
