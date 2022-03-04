ba10b9a67c936a0cf94de573d5137b7b0b59ac10
Matthias Monnier <matthias.monnier@gmail.com>
2022-03-04 10:48:35 +0100

    Add errorMiddleware and setup login and register routes

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,10 @@
+// Routes for /api/users/
+const express = require('express');
+const router = express.Router();
+const { registerUser, loginUser } = require('../controllers/userController');
+
+router.post('/', registerUser);
+
+router.post('/login', loginUser);
+
+module.exports = router;
---
+++ backend/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const dotenv = require('dotenv').config();
+const errorHandler = require('./middlewares/errorMiddleware');
+const PORT = process.env.PORT || 8000;
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Routes
+app.use('/api/users', require('./routes/userRoutes'));
+
+app.use(errorHandler);
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
